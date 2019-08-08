bafdeb41f0de7051f6c9f81a894984b28b7a2ae9
Ido-T <humbleboy03@gmail.com>
2019-08-08 23:26:11 +0100

    First commit

---
+++ app/src/main/java/com/example/instagramido/ParseApplication.java
@@ -0,0 +1,22 @@
+package com.example.instagramido;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ParseObject.registerSubclass(Post.class);
+        // set applicationId, and server server based on the values in the Heroku settings.
+        // clientKey is not needed unless explicitly configured
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("iberdo-instagram-codepath") // should correspond to APP_ID env variable
+                .clientKey("bwaswa03$myMasterKey")  // set explicitly unless clientKey is explicitly configured on Parse server
+                .server("http://iberdo-instagram-codepath.herokuapp.com/parse").build());
+    }
+}
